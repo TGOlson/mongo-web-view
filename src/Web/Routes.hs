71ef7476574b1cc6dce785301ccfbfa2215d321c
Tyler Olson <tydotg@gmail.com>
2015-06-06 12:51:30 -0700

    Fix aeson version conflict

--- src/Web/Routes.hs
+++ src/Web/Routes.hs
@@ -4,7 +4,6 @@ module Web.Routes where
 
 
 import Web.Scotty
-import Web.Utils
 import Web.Actions.Database
 import Control.Monad.Trans (liftIO)
 import Data.AesonBson
@@ -16,15 +15,15 @@ routes = do
 
   get "/databases" $ do
       dbs <- liftIO $ getAllDatabases "127.0.0.1"
-      toJsonResults dbs
+      json dbs
 
   get "/databases/:db" $ do
       db <- param "db"
       collections <- liftIO $ getAllCollections "127.0.0.1" db
-      toJsonResults collections
+      json collections
 
   get "/databases/:db/:collection" $ do
       db <- param "db"
       collection <- param "collection"
       docs <- liftIO $ getAllDocuments "127.0.0.1" db collection
-      json . show $ map aesonify docs
+      json $ map aesonify docs
---
+++ test/Integration.hs
@@ -0,0 +1,4 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+
+import System.Environment ( getArgs )
+import Test.Framework
