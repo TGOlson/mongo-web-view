a39fd305ba3fcc929b2fb31c2beebc495fda0301
Tyler Olson <tydotg@gmail.com>
2015-06-12 14:05:05 -0700

    Parse connection string

--- api/Server/Services/Parser.hs
+++ api/Server/Services/Parser.hs
@@ -1,1 +1,37 @@
 module Services.Parser where
+
+
+import Text.Parsec
+
+
+prefix :: Parsec String () String
+prefix = string "mongodb://"
+
+
+letters :: Parsec String () String
+letters = many1 letter
+
+
+parseHost :: Parsec String () String
+parseHost = do
+  domain <- letters
+  port   <- char ':' >> letters
+
+  return (domain ++ ":" ++ port)
+
+
+parser :: Parsec String () [String]
+parser = do
+  user     <- prefix   >> letters
+  password <- char ':' >> letters
+  host     <- char '@' >> parseHost
+  dbname   <- char '/' >> letters
+
+  return [user, password, host, dbname]
+
+
+-- Parse connection string
+-- "mongodb://user:password@domain:port/dbname"
+-- ["user", "password", "domain:port", "dbname"]
+parseConnectionString :: String -> Either ParseError [String]
+parseConnectionString = parse parser ""
--- api/Spec/Server/Services/ParserSpec.hs
+++ api/Spec/Server/Services/ParserSpec.hs
@@ -5,13 +5,33 @@ module Server.Services.ParserSpec where
 
 import Test.Hspec
 import Services.Parser
+import Data.List
+
 
 main :: IO ()
 main = hspec spec
 
 
+connectionString :: String
+connectionString = "mongodb://user:password@domain:port/dbname"
+
+
+shouldHaveParseError :: Show a => Either a b -> [String] -> Expectation
+shouldHaveParseError (Right _) _ = True `shouldBe` False
+shouldHaveParseError (Left e) err = show e `shouldBe` intercalate "\n" err
+
+
 spec :: Spec
-spec = do
-  describe "POST /collections" $ do
-    it "should return a list of collections for the specified database" $
-      True `shouldBe` True
+spec =
+  describe "parseConnectionString" $ do
+    it "should parse a valid connection string" $
+      parseConnectionString connectionString `shouldBe` Right ["user", "password", "domain:port", "dbname"]
+
+    it "should return an error when passed an invalid connection string" $ do
+      parseConnectionString "foo" `shouldHaveParseError` ["(line 1, column 1):",
+                                                        "unexpected \"f\"",
+                                                        "expecting \"mongodb://\""]
+
+      parseConnectionString "mongodb://foo@bar" `shouldHaveParseError` ["(line 1, column 14):",
+                                                                      "unexpected \"@\"",
+                                                                      "expecting letter or \":\""]
