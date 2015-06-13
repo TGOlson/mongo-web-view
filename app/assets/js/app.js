dbc41b2872444cd515a17818e781e09b05492c37
Tyler Olson <tydotg@gmail.com>
2015-06-12 23:23:04 -0700

    App

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,45 +1,94 @@
 angular.module('mwv', [
     'ngMaterial',
-    'ui.router'
+    'ui.router',
+    'vendor'
   ])
+
+.run(['$rootScope', function($rootScope) {
+
+  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+
+      console.error(error.stack || error);
+      throw new Error('$stateChangeError: ' + error.message);
+  });
+}])
 
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/');
-  //
-  // Now set up the states
+  $urlRouterProvider.otherwise('connect');
+
   $stateProvider
     .state('connect', {
       url: '/',
       templateUrl: '/templates/connect.html',
       controller: 'ConnectionCtrl as connectionCtrl'
+    })
+    .state('collections', {
+      url: '/collections',
+      templateUrl: '/templates/collections.html',
+      controller: 'CollectionsCtrl as collectionsCtrl'
+    })
+    .state('documents', {
+      url: '/collections/:collection',
+      templateUrl: '/templates/documents.html',
+      controller: 'DocumentsCtrl as documentsCtrl',
     });
 }])
 
 
-.controller('ConnectionCtrl', [function() {
+.controller('ConnectionCtrl', ['$state', 'ApiSvc', function($state, ApiSvc) {
   var connectionCtrl = this;
 
   console.log('Hello');
   console.log('Hello again');
 
-  connectionCtrl.connect = function(connectionString) {
-    console.log(connectionString);
+  connectionCtrl.connect = function(uri) {
+    ApiSvc.setMongoUri(uri);
+    $state.go('collections');
   };
 }])
 
-.factory('DbSvc', [function($http) {
+.controller('CollectionsCtrl', ['ApiSvc', function(ApiSvc) {
+  var collectionsCtrl = this;
 
-  var config = null;
+  collectionsCtrl.loading = true;
 
-  function setConfig(_config) {
-    config = _config;
+  ApiSvc.getCollections().then(function(collections) {
+    collectionsCtrl.collections = collections;
+    collectionsCtrl.loading = false;
+  });
+}])
+
+.controller('DocumentsCtrl', [function() {
+  var documentsCtrl = this;
+
+  console.log('docs');
+}])
+
+.factory('ApiSvc', ['$http', 'R', function($http, R) {
+
+  // var uri = null;
+
+  // Temp default uri for development
+  var uri = "mongodb://test-admin:password@ds043972.mongolab.com:43972/test-db";
+
+  function setMongoUri(_uri) {
+    uri = _uri;
   }
 
   function getCollections() {
-    $http.post('/collections', config).then(function(response) {
-      console.log(response);
-    });
+    return $http.post('/api/collections', {mongoUri: uri})
+      .then(R.prop('data'))
+      .catch(logApiError);
   }
+
+  function logApiError(err) {
+    console.log('ApiError', err);
+  }
+
+  return {
+    setMongoUri    : setMongoUri,
+    getCollections : getCollections
+  };
 
 }]);
